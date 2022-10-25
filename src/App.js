7a4849a9d3fa4eb960decdf05e9d27fabb9d4382
NabarunKar <nabarunkar01@gmail.com>
2022-10-26 00:56:52 +0530

    Added missing lines in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import React from "react";
+import PDFFile from './components/PDFFile';
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import ImagePicker from "./components/ImagePicker";
 
 const App = () => {
   return (
     <div className="App">
-      <ImagePicker />
+      <PDFDownloadLink document={<PDFFile />} filename="FORM">
+      {({loading}) => (loading ? <button>Loading Document...</button> : <button>Download</button> )}
+      </PDFDownloadLink>
+      {/* <PDFFile /> */}
     </div>
   );
 };
